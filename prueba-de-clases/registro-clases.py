f403c1ee1754fd5b3abcad8125913f409714d9e7
Carlos Cardozo <carjaviercardozo0@gmail.com>
2023-12-08 15:25:46 -0400

    logs

--- prueba-de-clases/registro-clases.py
+++ prueba-de-clases/registro-clases.py
@@ -1,5 +1,16 @@
+# bibliotecas
+from pathlib import Path
 import pickle
+import logging
 
+
+# manejador de archivo para guardar los logs en un archivo de texto
+logger = logging.getLogger("MiApp")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("registro.log")
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")) # Puedes cambiar el formato de los logs según tus preferencias
+
+# clase con atributos y metodos
 class Usuario:
     def __init__(self, nombre, apellido, ID):
         self.nombre = nombre
@@ -10,7 +21,7 @@ class Usuario:
         # Definimos un método para mostrar los datos del usuario de forma amigable
         return f"{self.nombre} {self.apellido} ({self.ID})"
 
-
+#declaracion de funciones
 def registrar_usuario():
     nombre = input("Ingrese el nombre del usuario: ")
     apellido = input("Ingrese el apellido del usuario: ")
@@ -22,12 +33,13 @@ def registrar_usuario():
     pickle.dump(usuario, archivo)
     
     archivo.close()
+    logger.info("Escritura en el archivo")
     print(f"El usuario {usuario} ha sido registrado con éxito.")
 
-def mostrar_usuarios():
-   
+def leer_usuarios():
+    
     archivo = open("usuarios.bin", "rb")
-
+   
     #lista para almacenar usuarios
     usuarios = []
 
@@ -45,13 +57,20 @@ def mostrar_usuarios():
     
     archivo.close()
 
-    print(f"Se han encontrado {len(usuarios)} usuarios registrados:")
-    for usuario in usuarios:
-        print(usuario)
+    if len(usuarios) is 0:
+        print(f"No se encontró ningún usuario")
+        logger.error("No se pudo leer el registro o registro vacio")
+    else:
+        print(f"Se han encontrado {len(usuarios)} usuarios registrados:")
+        for usuario in usuarios:
+            print(usuario)
+            logger.info("Lectura del archivo")
 
 def buscar_usuario():
     ID = input("Ingrese el ID del usuario a buscar: ")
+    
     archivo = open("usuarios.bin", "rb")
+  
 
     #variable para almacenar el usuario encontrado (o None si no se encuentra)
     usuario_encontrado = None
@@ -75,6 +94,7 @@ def buscar_usuario():
     if usuario_encontrado is None:
         print(f"No se encontró ningún usuario con el ID {ID}.")
     else:
+        logger.info("Busqueda en el archivo")
         print(f"Se encontró el usuario con el ID {ID}:")
         print(usuario_encontrado)
 
@@ -83,6 +103,7 @@ def borrar_usuario():
     ID = input("Ingrese el ID del usuario a borrar: ")
 
     archivo = open("usuarios.bin", "rb")
+
 
     #lista vacía para almacenar los usuarios que no se van a borrar
     usuarios = []
@@ -109,6 +130,7 @@ def borrar_usuario():
     if usuario_borrado is None:
         print(f"No se encontró ningún usuario con el ID {ID}.")
     else:
+        logger.info("Borrado en el archivo")
         print(f"El usuario {usuario_borrado} ha sido borrado con éxito.")
 
         # Abrimos el archivo binario en modo write (para sobreescribir)
@@ -123,7 +145,7 @@ def borrar_usuario():
 def ordenar_usuarios():
    
     archivo = open("usuarios.bin", "rb")
-  
+
     usuarios = []
   
     while True:
@@ -146,14 +168,39 @@ def ordenar_usuarios():
         pickle.dump(usuario, archivo)
     archivo.close()
 
+# variables globales
 GV_Seleccion = 1
+GV_Log_switch = 0
+
+# crear archivo "usuarios" en caso de no exisir
+myfile = Path("usuarios.bin")
+myfile.touch(exist_ok=True)
+
+# activar o desactivar log
+while GV_Log_switch != 1 and GV_Log_switch != 2:
+    GV_Log_switch = int(input("\nDesea activar el log de acciones?:1=si|2=no\n"))
+    if GV_Log_switch == 1:
+        LOGGING_ENABLED = True
+        print("Logs activados")
+    elif GV_Log_switch == 2:
+        LOGGING_ENABLED = False
+        print("Logs desactivados")
+    else:
+        print("valor no compatible")
+
+
+# confirmar si el log esta activo o no y hacerlo funcionar
+if LOGGING_ENABLED:
+    logger.addHandler(file_handler)
+
+# menu de funciones
 while GV_Seleccion != 0:
 
     ordenar_usuarios()
 
     GV_Seleccion = int(input("\nQue Operaacion Desea Relizar El Dia De Hoy?\n 1=leer|2=buscar|3=escribir|4=borrar|0=salir :"))
     if(GV_Seleccion == 1):
-        mostrar_usuarios()
+        leer_usuarios()
     elif(GV_Seleccion == 2):
         buscar_usuario()
     elif(GV_Seleccion == 3):
